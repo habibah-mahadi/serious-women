1a2c5f6154eb0f8dac53d37536251184d53d02e7
habibah-mahadi <habibah.hm701@gmail.com>
2022-08-07 12:18:21 +0800

    code splitting with webpack

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,13 +2,37 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
+// import Modal from './modules/Modal';
 
-new Modal();
-let stickyHeader = new StickyHeader();
-let mobileMenu = new MobileMenu();
+// new Modal();
+new StickyHeader();
+new MobileMenu();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75); //element should begin when it crosses the bottom 25% of the browser viewport
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60); //this will come a bit later, because it needs 40% of the element from the bottom to be visible 
+
+//code splitting with webpack
+let modal; //initially as undefined
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+
+        //check if the modal file is already downloaded or not
+        if (typeof modal == 'undefined') {
+
+            //make promise
+            import(/* webpackChunkName: "bibahmodal" */ './modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(()=> modal.openTheModal(), 20);
+            }
+            ).catch(()=> console.log("There was a problem."))
+
+        } else {
+            modal.openTheModal();
+        }
+
+    })
+})
+
 
 if (module.hot) {
     module.hot.accept();
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,13 +3,13 @@ class Modal {
         this.injectHTML();
         this.modal = document.querySelector(".modal");
         this.closeIcon = document.querySelector(".modal__close");
-        this.openModalButtons = document.querySelectorAll(".open-modal");
+        // this.openModalButtons = document.querySelectorAll(".open-modal");
         this.events();
     }
 
     events() {
         //listen for opening click
-        this.openModalButtons.forEach(el => el.addEventListener("click", e => this.openTheModal(e)))
+        // this.openModalButtons.forEach(el => el.addEventListener("click", e => this.openTheModal(e)))
 
         //listen for close click
         this.closeIcon.addEventListener("click", ()=> this.closeTheModal());
@@ -19,8 +19,7 @@ class Modal {
 
     }
 
-    openTheModal(e) {
-        e.preventDefault();
+    openTheModal() {
         this.modal.classList.add("modal--is-visible");
     }
 
